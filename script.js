b9ea82b765d0a296c5ce862cd933c481ad659e70
monik-grey <monicaehonwa@gmail.com>
2026-02-21 11:09:35 +0100

    Added new html pages and features and removed some features

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-const headerEl = document.querySelector('.header');
 
-window.addEventListener('scroll', () => {
-  if(window.scrollY > 50) {
-    header.classList.add('scrolled');
-  } else {
-    header.classList.remove('scrolled');
-  }
-});
